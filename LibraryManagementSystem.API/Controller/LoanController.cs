99bfff6b53e878ed495e7d02b1191617bc3e9993
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-16 16:23:06 -0400

    additional loan methods created

--- LibraryManagementSystem.API/Controller/LoanController.cs
+++ LibraryManagementSystem.API/Controller/LoanController.cs
@@ -7,6 +7,24 @@ namespace LibraryManagementSystem.API.Controller
     [Route("api/loan")]
     public class LoanController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult LoanGetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult LoanGetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult LoanGetByUserId(int userId)
+        {
+            return Ok();
+        }
+        
         [HttpPost]
         public IActionResult LoanCreate([FromBody] LoanModel newLoan)
         {
@@ -16,6 +34,27 @@ namespace LibraryManagementSystem.API.Controller
             }
 
             return CreatedAtAction(nameof(LoanCreate), newLoan);
+        }
+
+        [HttpPost]
+        public IActionResult LoanUpdate([FromBody] LoanModel updateLoan)
+        {
+            if(updateLoan == null)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(LoanCreate), updateLoan);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult LoanEnd(int id)
+        {
+            //Look for loan 
+            //If it exists go ahead and end it 
+            // otherwise return to different page
+
+            return NoContent();
         }
     }
 }
