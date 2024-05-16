54ebe1b5d7f90a07dc808e5c9a4346ff43fc2dcb
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-16 16:24:55 -0400

    properties added

--- LibraryManagementSystem.Application/ViewModels/BookCreateModel.cs
+++ LibraryManagementSystem.Application/ViewModels/BookCreateModel.cs
@@ -2,5 +2,8 @@ namespace LibraryManagementSystem.Application.ViewModels;
 
 public class BookCreateModel
 {
-    
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISNB { get; set; }
+    public int PublicationYear { get; set; }
 }
--- LibraryManagementSystem.Application/ViewModels/UserCreateModel.cs
+++ LibraryManagementSystem.Application/ViewModels/UserCreateModel.cs
@@ -2,5 +2,6 @@ namespace LibraryManagementSystem.Application.ViewModels;
 
 public class UserCreateModel
 {
-    
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
