d861b57cd8d345d64e7469062479495269c95e93
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-15 21:24:48 -0400

    database context

---
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFreela.Core.Entities
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    internal class DevFreelaDbContext
+    {
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
