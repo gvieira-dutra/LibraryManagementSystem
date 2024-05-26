2998035bdb272dd0f006c2fe5144ff1a2a684e1d
gleeeeison <gvieira-durta@myseneca.ca>
2024-05-26 18:52:39 -0400

    del

--- LibraryManagementSystem.Application/ViewModels/BookCreateModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LibraryManagementSystem.Application.ViewModels;
-
-public class BookCreateModel
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string ISNB { get; set; }
-    public int PublicationYear { get; set; }
-}
