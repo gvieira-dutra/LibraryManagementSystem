ef35c929719e1b893603b3bf7fa21aacfcce0c79
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-03 08:16:35 -0400

    EF Core setup

--- LibraryManagementSystem.Infrastructure/Persistence/LibMgmtSysDbContext.cs
+++ LibraryManagementSystem.Infrastructure/Persistence/LibMgmtSysDbContext.cs
@@ -1,34 +1,22 @@
 ﻿using LibraryManagementSystem.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace LibraryManagementSystem.Infrastructure.Persistence
 {
-    public class LibMgmtSysDbContext
+    public class LibMgmtSysDbContext : DbContext
     {
-        public LibMgmtSysDbContext()
+        public LibMgmtSysDbContext(DbContextOptions<LibMgmtSysDbContext> options) : base(options)
         {
-            Books = new List<Book>
-            {
-                new Book("Lord of the Rings", "Mr Token", "11111", 2000),
-                new Book("Harry Potter", "JK Rolling", "22222", 2001),
-                new Book("Da Vinci Code", "Tom Hanks", "33333", 2004)
-            };
-
-            Users = new List<User>
-            {
-                new User("RobS", "Rob Souza" , "rs@mail.com"),
-                new User("AdriD", "Adri De", "ad@mail.com"),
-                new User("GleiV", "Glei Vieira", "gv@mail.com")
-            };
-
         }
 
-        public List<Book> Books { get; set; }
-        public List<Loan> Loans { get; set; }
-        public List<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
