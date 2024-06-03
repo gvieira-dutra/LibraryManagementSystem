e213a4000f08179781453b975a6f8d4c060ac921
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-03 08:19:24 -0400

    modify index

--- LibraryManagementSystem.Core/Enums/BookStatus.cs
+++ LibraryManagementSystem.Core/Enums/BookStatus.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LibraryManagementSystem.Core.Enums
 {
     public enum BookStatus
     {
-        Unavailable = 0,
-        Available = 1,
-        NotInTheSystem = 2,
+        Unavailable = 1,
+        Available = 2,
+        NotInTheSystem = 3,
     }
 }
