f438279d313ca59b75f3ec2f440f9982b8b10aa6
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-03 08:19:56 -0400

    added loans list to a book

--- LibraryManagementSystem.Application/ViewModels/LoanViewModel.cs
+++ LibraryManagementSystem.Application/ViewModels/LoanViewModel.cs
@@ -8,17 +8,23 @@ namespace LibraryManagementSystem.Application.ViewModels
 {
     public class LoanViewModel
     {
-        public LoanViewModel(int id, int idUser, int idBook, DateTime loanDate)
+        public LoanViewModel(int id, int idUser, int idBook, DateTime loanDate, string status, string user, string bookTitle)
         {
             Id = id;
             IdUser = idUser;
             IdBook = idBook;
             LoanDate = loanDate;
+            Status = status;
+            BookTitle = bookTitle;
+            UserFullName = user;
         }
 
         public int Id { get; set; }
         public int IdUser { get; set; }
         public int IdBook { get; set; }
         public DateTime LoanDate { get; set; }
+        public string Status { get; set; }
+        public string BookTitle { get; set; }
+        public string UserFullName {  get; set; }
     }
 }
