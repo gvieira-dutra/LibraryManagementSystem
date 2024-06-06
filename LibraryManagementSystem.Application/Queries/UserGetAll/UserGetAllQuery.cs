4521f15b2100abcd1cb4ca7ab1008deb011e2c4b
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:09:15 -0400

    cors user get all

---
+++ LibraryManagementSystem.Application/Queries/UserGetAll/UserGetAllQuery.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application.Queries.UserGetAll
+{
+    public class UserGetAllQuery : IRequest<List<UserViewModel>>
+    {
+        public UserGetAllQuery(string query)
+        {
+            this.query = query;
+        }
+        public string query { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/UserGetAll/UserGetAllQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace LibraryManagementSystem.Application.Queries.UserGetAll
+{
+    public class UserGetAllQueryHandler : IRequestHandler<UserGetAllQuery, List<UserViewModel>>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+        private readonly string _connectionString;
+
+        public UserGetAllQueryHandler(LibMgmtSysDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("LibraryMgmtSysCs");
+        }
+        public async Task<List<UserViewModel>> Handle(UserGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var users = _dbContext.Users.AsQueryable();
+
+            if (!string.IsNullOrEmpty(request.query))
+            {
+                users = users
+                        .Where(u => u.FullName.Contains(request.query));
+            }
+
+            var userVM = await users.Select(u => new UserViewModel(u.Username, u.FullName, u.Email)).ToListAsync();
+
+            return userVM;
+        }
+    }
+}
