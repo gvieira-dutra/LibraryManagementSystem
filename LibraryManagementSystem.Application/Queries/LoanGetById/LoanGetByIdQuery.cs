9bc8b03d023dedce8c46cdaff29090960ec0a5e1
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:59 -0400

    cors loan get by id

---
+++ LibraryManagementSystem.Application/Queries/LoanGetById/LoanGetByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿
+
+using LibraryManagementSystem.Application.Queries.LoanGetAll;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.LoanGetById
+{
+    public class LoanGetByIdQuery : IRequest<LoanDetailViewModel>
+    {
+        public LoanGetByIdQuery(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/LoanGetById/LoanGetByIdQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using LibraryManagementSystem.Application.Queries.LoanGetAll;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Application.Queries.LoanGetById
+{
+    public class LoanGetByIdQueryHandler : IRequestHandler<LoanGetByIdQuery, LoanDetailViewModel>
+    {
+
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public LoanGetByIdQueryHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<LoanDetailViewModel> Handle(LoanGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var loan = await _dbContext.Loans
+           .Include(l => l.Book)
+           .Include(l => l.User)
+           .SingleOrDefaultAsync(l => l.Id == request.Id);
+
+            if (loan != null)
+            {
+                loan.LoanCheckLate();
+
+                var loanVM = new LoanDetailViewModel(
+                    loan.Id,
+                    loan.UserId,
+                    loan.BookId,
+                    loan.LoanStartDate,
+                    loan.LoanCurrStatus.ToString(),
+                    loan.Book.Title,
+                    loan.User.FullName);
+
+                return loanVM;
+            }
+
+            return null;
+        }
+    }
+}
