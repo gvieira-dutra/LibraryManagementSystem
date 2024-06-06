2c68cbfdf5cbb366b2dfe47bb7ff3076821199f9
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:29 -0400

    cors book get all

---
+++ LibraryManagementSystem.Application/Queries/BookGetAll/BookGetAllQuery.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.BookGetAll
+{
+    public class BookGetAllQuery : IRequest<List<BookViewModel>>
+    {
+        public BookGetAllQuery(string query)
+        {
+            this.query = query;
+        }
+
+        public string query { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/BookGetAll/BookGetAllQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using LibraryManagementSystem.Core.Enums;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace LibraryManagementSystem.Application.Queries.BookGetAll
+{
+    public class BookGetAllQueryHandler : IRequestHandler<BookGetAllQuery, List<BookViewModel>>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public BookGetAllQueryHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<BookViewModel>> Handle(BookGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var books = _dbContext.Books.AsQueryable();
+
+            books = books.Where(b => b.Availability != BookStatus.NotInTheSystem);
+
+            if (!string.IsNullOrEmpty(request.query))
+            {
+                books = books.Where(b => b.Title.Contains(request.query));
+            }
+
+            var booksVM = await books.Select(b => new BookViewModel(b.Title, b.Author, b.PublicationYear))
+                        .ToListAsync();
+
+            return booksVM;
+        }
+    }
+}
