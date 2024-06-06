7f6f89e971b07f164586076c48758198d1d9a5f8
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:38 -0400

    cors book get one

---
+++ LibraryManagementSystem.Application/Queries/BookGetOne/BookGetOneQuery.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.BookGetOne
+{
+    public class BookGetOneQuery : IRequest<BookViewModel>
+    {
+        public BookGetOneQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/BookGetOne/BookGetOneQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagementSystem.Application.ViewModels;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace LibraryManagementSystem.Application.Queries.BookGetOne
+{
+
+    public class BookGetOneQueryHandler : IRequestHandler<BookGetOneQuery, BookViewModel>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+        public BookGetOneQueryHandler(LibMgmtSysDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<BookViewModel> Handle(BookGetOneQuery request, CancellationToken cancellationToken)
+        {
+            var book = await _dbContext.Books.SingleOrDefaultAsync(b => b.Id == request.Id);
+
+            var bookViewModel = new BookViewModel(book.Title, book.Author, book.PublicationYear);
+
+            return bookViewModel ;
+        }
+    }
+}
