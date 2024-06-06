effb339dc58e8201fbd945310da04a4fe68e784a
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:07:54 -0400

    CORS loan create

---
+++ LibraryManagementSystem.Application/Commands/LoanCreate/LoanCreateCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.LoanCreate
+{
+    public class LoanCreateCommand : IRequest<int>
+    {
+        public int IdUser { get; set; }
+        public int IdBook { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Commands/LoanCreate/LoanCreateCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.LoanCreate
+{
+    public class LoanCreateCommandHandler : IRequestHandler<LoanCreateCommand, int>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public LoanCreateCommandHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(LoanCreateCommand request, CancellationToken cancellationToken)
+        {
+            var book = _dbContext.Books.SingleOrDefault(b => b.Id == request.IdBook);
+
+            if (book != null && book.Availability == Core.Enums.BookStatus.Available)
+            {
+                var loan = new Loan(request.IdUser, request.IdBook);
+                await _dbContext.Loans.AddAsync(loan);
+
+                book.BookSetUnavailable();
+                await _dbContext.SaveChangesAsync();
+
+                return loan.Id;
+            }
+
+            return 0;
+        }
+    }
+}
