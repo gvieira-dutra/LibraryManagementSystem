7e6401fc208e6d8e8d592d715f4c9e4f548ca62d
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:09:08 -0400

    cors loan get by user id

---
+++ LibraryManagementSystem.Application/Queries/LoanGetByUserId/LoanGetByUserIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementSystem.Application.Queries.LoanGetAll;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Queries.LoanGetByUserId
+{
+    public class LoanGetByUserIdQuery : IRequest<List<LoanDetailViewModel>>
+    {
+        public LoanGetByUserIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Queries/LoanGetByUserId/LoanGetByUserIdQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using LibraryManagementSystem.Application.Queries.LoanGetAll;
+using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Application.Queries.LoanGetByUserId
+{
+    public class LoanGetByUserIdQueryHandler : IRequestHandler<LoanGetByUserIdQuery, List<LoanDetailViewModel>>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public LoanGetByUserIdQueryHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<LoanDetailViewModel>> Handle(LoanGetByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var loans = await _dbContext.Loans.Where(l => l.UserId == request.Id)
+                       .Include(l => l.Book)
+                       .Include(l => l.User)
+                       .ToListAsync();
+
+            if (loans.Any())
+            {
+
+                foreach (var l in loans)
+                {
+                    l.LoanCheckLate();
+                }
+
+                var loansVM = loans.Select(l => new LoanDetailViewModel(
+                   l.Id,
+                   l.UserId,
+                   l.BookId,
+                   l.LoanStartDate,
+                   l.LoanCurrStatus.ToString(),
+                   l.Book.Title,
+                   l.User.FullName))
+                   .ToList();
+
+                return loansVM;
+            }
+            return null;
+        }
+    }
+}
