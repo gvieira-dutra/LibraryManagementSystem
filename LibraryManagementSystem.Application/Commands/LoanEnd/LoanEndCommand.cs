0e72f4f0f1851c33ee9f2efdcd3f1fa474dcb96c
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:04 -0400

    cors loan end

---
+++ LibraryManagementSystem.Application/Commands/LoanEnd/LoanEndCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.LoanEnd
+{
+    public class LoanEndCommand : IRequest<string>
+    {
+        public LoanEndCommand(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; private set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Commands/LoanEnd/LoanEndCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Application.Commands.LoanEnd
+{
+    public class LoanEndCommandHandler : IRequestHandler<LoanEndCommand, string>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public LoanEndCommandHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<string> Handle(LoanEndCommand request, CancellationToken cancellationToken)
+        {
+
+            var loan = _dbContext.Loans
+                .Include(l => l.Book)
+                .SingleOrDefault(l => l.Id == request.Id);
+
+
+            var message = "";
+
+            if (loan != null)
+            {
+                loan.LoanCheckLate();
+
+                if (loan.LoanCurrStatus == Core.Enums.LoanStatus.Late)
+                {
+                    message += "This book is late!";
+                }
+                else
+                {
+                    message += "Thank you for returning on time!";
+                }
+
+                loan.LoanSetReturned();
+                loan.Book.BookSetAvailable();
+
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return message;
+        }
+    }
+}
