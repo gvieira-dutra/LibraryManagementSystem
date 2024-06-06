f98c406da571dbbc65e441611e61c3ba15845a7a
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:12 -0400

    cors loan update

---
+++ LibraryManagementSystem.Application/Commands/LoanUpdate/LoanUpdateCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.LoanUpdate
+{
+    public class LoanUpdateCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public int IdBook { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Commands/LoanUpdate/LoanUpdateCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Application.Commands.LoanUpdate
+{
+    public class LoanUpdateCommandHandler : IRequestHandler<LoanUpdateCommand, int>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public LoanUpdateCommandHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(LoanUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var loan = await _dbContext.Loans.SingleOrDefaultAsync(l => l.Id == request.Id);
+
+            if (loan == null) { return 0; }
+
+            loan.Update(request.IdUser, request.IdBook);
+            await _dbContext.SaveChangesAsync();
+
+            return loan.Id;
+        }
+    }
+}
