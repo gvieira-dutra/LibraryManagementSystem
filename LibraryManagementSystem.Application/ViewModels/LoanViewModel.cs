5db0850921ddd814fae6cd01e94592f2687a3742
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:09:52 -0400

    loan view model updated

--- LibraryManagementSystem.Application/ViewModels/LoanViewModel.cs
+++ LibraryManagementSystem.Application/ViewModels/LoanViewModel.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LibraryManagementSystem.Application.ViewModels
 {
     public class LoanViewModel
     {
-        public LoanViewModel(int id, int idUser, int idBook, DateTime loanDate, string status, string user, string bookTitle)
+        public LoanViewModel(int id, int idUser, int idBook, string bookTitle)
         {
             Id = id;
             IdUser = idUser;
             IdBook = idBook;
-            LoanDate = loanDate;
-            Status = status;
             BookTitle = bookTitle;
-            UserFullName = user;
         }
 
         public int Id { get; set; }
         public int IdUser { get; set; }
         public int IdBook { get; set; }
-        public DateTime LoanDate { get; set; }
-        public string Status { get; set; }
         public string BookTitle { get; set; }
-        public string UserFullName {  get; set; }
     }
 }
