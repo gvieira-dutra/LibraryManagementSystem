0b44ff3285c4571b9f35b1654c73df6b49e7db95
gleeeeison <gvieira-durta@myseneca.ca>
2024-06-06 07:08:20 -0400

    cors user create

---
+++ LibraryManagementSystem.Application/Commands/UserCreate/UserCreateCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.UserCreateNew
+{
+    public class UserCreateCommand : IRequest<int>
+    {
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Application/Commands/UserCreate/UserCreateCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Infrastructure.Persistence;
+using MediatR;
+
+namespace LibraryManagementSystem.Application.Commands.UserCreateNew
+{
+    public class UserCreateCommandHandler : IRequestHandler<UserCreateCommand, int>
+    {
+        private readonly LibMgmtSysDbContext _dbContext;
+
+        public UserCreateCommandHandler(LibMgmtSysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(UserCreateCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User(request.Username, request.FullName, request.Email);
+
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return user.Id;
+        }
+    }
+}
